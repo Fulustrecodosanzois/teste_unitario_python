7a3d4b051eab5a39cc73ed7b5e7946a2504b630b
Luciano Silva <luciano_jet@hotmail.com>
2023-10-05 20:56:38 -0300

    feito classes

---
+++ app/funcionario.py
@@ -0,0 +1,27 @@
+from datetime import date
+
+class Funcionario: 
+    def __init__(self, nome, cargo, salario):
+        self.nome = nome
+        self.cargo = cargo
+        self.salario = salario
+    def dados(self):
+        return f"{self.nome} seu cargo é {self.cargo} e seu salário é {self.salario}"
+    
+    def registrarPonto(self):
+        return f"Ponto registrado na data {date.today()}"
+
+    def descontarSalario(self, valor):
+        if valor < 0:
+            return "Operação inválida"
+        else:
+            self.salario -= valor
+
+    def promover(self, novoCargo, novoSalario):
+        self.cargo= novoCargo
+        self.salario= novoSalario
+        return f"promovido para {self.cargo} com salario de R$ {self.salario}"
+    
+    def calcularBonus(self):
+        bonus = self.salario * 0.1 #claculo do salario
+        return bonus
---
+++ app/main.py
@@ -0,0 +1,13 @@
+from funcionario import Funcionario
+
+f1 = Funcionario("Pedo", "cafetão", 10000)
+
+# print(f1.dados())
+
+# print(f1.registrarPonto())
+
+# f1.descontarSalario(500)
+# print(f1.dados())
+
+print(f1.promover("Pastor", 305000))
+print(f1.calcularBonus())
